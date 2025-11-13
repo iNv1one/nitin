28f74851700f232de6288cabe34546dce290204e
iNv1one <Invictus6022@yandex.ru>
2025-11-13 11:39:08 +0300

    Исправлен LOGIN_URL: теперь обычные пользователи перенаправляются на /login/ вместо /admin/login/, также LOGOUT_REDIRECT_URL изменен на /login/

--- config/settings.py
+++ config/settings.py
@@ -115,9 +115,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login URLs
-LOGIN_URL = '/admin/login/'
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Telegram settings
 TELEGRAM_API_ID = config('TELEGRAM_API_ID', cast=int)
